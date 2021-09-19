c2ce549151610367abefed9e4ec2a5730f89564e
George <drpresq@gmail.com>
2021-09-19 16:16:41 -0400

    Updated main routine to include automatic absolute path resolution

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pyqrp',
-    version="0.6",
+    version="0.7",
     packages=["pyqrp"],
     package_dir={'': "src"},
     scripts=['scripts/pyqrp'],
